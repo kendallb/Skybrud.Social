d95a1ea1432d2e86090e291ee218cc31d5725f42
Kendall Bennett <kendallb@amain.com>
2015-01-30 18:17:00 -0800

    Added batch files to copy files over

---
+++ copy_debug.bat
@@ -0,0 +1,2 @@
+@echo off
+copy src\Skybrud.Social\bin\Debug\Skybrud.Social.* c:\src\snap\ExternalAssemblies
---
+++ copy_release.bat
@@ -0,0 +1,2 @@
+@echo off
+copy src\Skybrud.Social\bin\Release\Skybrud.Social.* c:\src\snap\ExternalAssemblies
